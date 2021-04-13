b8ed10f384f81328e9751baaae79edf3ed6962de
Philip Tzou <philip.npc@gmail.com>
2021-04-13 13:06:07 -0700

    Add toString() to UnsequencedRegion object

--- src/main/java/edu/stanford/hivdb/sequences/UnsequencedRegions.java
+++ src/main/java/edu/stanford/hivdb/sequences/UnsequencedRegions.java
@@ -11,22 +11,30 @@ import edu.stanford.hivdb.viruses.Virus;
 import edu.stanford.hivdb.viruses.WithGene;
 
 public class UnsequencedRegions<VirusT extends Virus<VirusT>> implements WithGene<VirusT> {
-	
+
 	public static class UnsequencedRegion {
 		private final Long posStart;
 		private final Long posEnd;
-		
+
 		protected UnsequencedRegion(long posStart, long posEnd) {
 			this.posStart = posStart;
 			this.posEnd = posEnd;
 		}
-		
+
 		public Long getPosStart() { return posStart; }
-			
+
 		public Long getPosEnd() { return posEnd; }
 
 		public Long getSize() { return posEnd - posStart + 1; }
-		
+
+		public Long size() { return getSize(); }
+
+		@Override
+		public String toString() {
+			return String.format("%d-%d", posStart, posEnd);
+		}
+
+
 	}
 
 	private static <T extends Virus<T>> List<UnsequencedRegion> findUnseqRegionsFromMutations(Gene<T> gene, MutationSet<T> mutations) {
@@ -53,10 +61,10 @@ public class UnsequencedRegions<VirusT extends Virus<VirusT>> implements WithGen
 		}
 		return regions;
 	}
-	
+
 	private final Gene<VirusT> gene;
 	private final List<UnsequencedRegion> regions;
-	
+
 	public UnsequencedRegions(Gene<VirusT> gene, long firstAA, long lastAA, MutationSet<VirusT> mutations) {
 		List<UnsequencedRegion> regions = new ArrayList<>();
 		if (firstAA > 1) {
@@ -69,14 +77,18 @@ public class UnsequencedRegions<VirusT extends Virus<VirusT>> implements WithGen
 		this.gene = gene;
 		this.regions = Collections.unmodifiableList(regions);
 	}
-	
+
 	@Override
 	public Gene<VirusT> getGene() { return gene; }
 
 	public List<UnsequencedRegion> getRegions() { return regions; }
-	
+
 	public Long getSize() {
 		return regions.stream().map(r -> r.getSize()).reduce(0L, (a, b) -> a + b);
 	}
-	
+
+	public Long size() {
+		return getSize();
+	}
+
 }
